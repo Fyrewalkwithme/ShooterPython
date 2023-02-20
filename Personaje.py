c703dc1369ce3e068e770081379ddc14e78b594c
Fyrewalkwithme <loeza.garcia.luisarturo@gmail.com>
2023-02-20 17:00:08 -0600

    Agregaron métodos de pj

--- Personaje.py
+++ Personaje.py
@@ -3,3 +3,17 @@ class Personaje:
     nombre = "Master Chief"
     altura = "2.70"
     
+    def correr(self, sprint):
+        if(sprint):
+            print("Sprint activado para " + self.nombre)
+        else:
+            print("Sprint desactivado para " + self.nombre)  
+              
+    def lanzarGranadas(self):
+        print(self.nombre + "Lanzo una granada.")
+         
+    def recarga(self, ammo):
+        mag = 10
+        mag = mag+ammo
+        print(self.nombre + " cargó "+ammo+" balas.")
+        print("Municion actual: "+mag+" balas.")
